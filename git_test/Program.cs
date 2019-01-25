614879849e915b749622b1e857ad4b63211b1983
ALi860713 <w11160212@gmail.com>
2019-01-26 00:20:23 +0800

    begin add register function

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -10,6 +10,7 @@ namespace git_test
     {
         static void Main(string[] args)
         {
+            var test = "a+b";
             var a = "a";
             var b = "b";
             Console.Write(a + b);
